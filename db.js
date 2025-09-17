cc96bc2bdf07d11fc8d63b86c04e7b0b4bf66c42
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-09-17 10:46:51 +0500

    Implement login

---
+++ db.js
@@ -0,0 +1,27 @@
+export const users = {
+	najimov: {
+		password: "123",
+		isAdmin: true,
+		name: "Muhammadxon Najimov",
+	},
+	firdavs: {
+		password: "123",
+		isAdmin: false,
+		name: "Firdavs Normurodov",
+	},
+	asror: {
+		password: "123",
+		isAdmin: false,
+		name: "Asror Qurbonnazarov",
+	},
+	alisher: {
+		password: "123",
+		isAdmin: false,
+		name: "Alisher Abdusalomov",
+	},
+	javohir: {
+		password: "123",
+		isAdmin: false,
+		name: "Javohir Qo'ldoshev",
+	}
+}
--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 import express from "express"
 import cors from "cors"
+import jwt from "jsonwebtoken"
+import * as DB from "./db.js"
 
-const SECRET = "hello_world"
+const JWT_SECRET = "hello_world"
+
+// const data = {
+// 	username: "najimov",
+// 	isAdmin: true,
+// }
+
+// const token = jwt.sign( data, SECRET, { expiresIn: "1h" } )
 
 const server = express()
 
 server.use( cors() )
+server.use( express.json() )
 
-server.get( "/secret-data", ( req, res ) => {
+server.post( "/login", ( req, res ) => {
 
-	if ( req.headers.token === SECRET ) {
+	const { username, password } = req.body
 
-		res.send( {
-			secret: "data",
-		} )
+	if ( !username || !password ) {
+
+		res.status( 400 ).end()
+		return
 	}
-	else {
+
+	if ( !DB.users[ username ] ) {
 
 		res.status( 401 ).end()
+		return
 	}
+
+	const user = DB.users[ username ]
+
+	if ( user.password !== password ) {
+
+		res.status( 401 ).end()
+		return
+	}
+
+	const payload = {
+		username,
+		isAdmin: user.isAdmin,
+	}
+
+	const token = jwt.sign( payload, JWT_SECRET, { expiresIn: "1h" } )
+
+	res.status( 201 ).send( {
+		username,
+		name: user.name,
+		token,
+	} )
 } )
 
 server.listen( 3000, () => console.info( ":3000" ) )
